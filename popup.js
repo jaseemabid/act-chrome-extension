c2c271ebc13eb9a0c7cdd35f201ea23570c3901c
Tenzin Kunkyab <ten.kunkyab@gmail.com>
2014-12-20 10:34:45 +0530

    Changed to small from capital error message

--- popup.js
+++ popup.js
@@ -66,7 +66,7 @@ packageHandler.onreadystatechange = function () {
         fup; // Users's package -> one among the list
 
     if (this.readyState !== 4 || this.status !== 200) {
-	log('error', 'NOT INSIDE ACT BROADBAND');
+	log('error', 'Not within ACT Broadband');
         return this;
     }
 
